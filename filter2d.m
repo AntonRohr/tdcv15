ea67249b7ac09b85b45104ce3fc139cb273d9708
atroyn <anton.troynikov@gmail.com>
2015-10-27 17:31:49 +0100

    Previously we were just lucky the filter was symmetric and we always got a symmetric chunk. It turns out output(i,j) actually indexes into the ith row, jth column - we had it backwards in two places. Also renamed some variables.

--- filter2d.m
+++ filter2d.m
@@ -1,7 +1,7 @@
 function output = filter2d(input, mask, border_treatment)
 
-[M, N] = size(mask);
-padX = floor(M/2); padY = floor(N/2);
+[y_size, x_size] = size(mask);
+padX = floor(x_size/2); padY = floor(y_size/2);
 
 %Initialize the output
 output = zeros(size(input));
@@ -12,9 +12,9 @@ padded_image = pad(input, padX, padY, border_treatment);
 %Do convolution
 %Each pixel in the output is the convolution of pixels in the padded image
 %with the filter mask.
-for i = 1:size(output,1)
-    for j = 1:size(output,2)
-        filter_area = padded_image(i:i+N-1,j:j+M-1);
-        output(i,j) = sum(sum(filter_area.*mask));
+for i = 1:size(output,2)
+    for j = 1:size(output,1)
+        filter_area = padded_image(j:j+y_size-1,i:i+x_size-1);
+        output(j,i) = sum(sum(filter_area.*mask));
     end
 end
