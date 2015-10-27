3aa3515c7c306057464fbf154d26f5fffdb3681e
atroyn <anton.troynikov@gmail.com>
2015-10-27 17:46:13 +0100

    Implemented combination of 1D normals. Doesn't really work.

--- exercise1_2.m
+++ exercise1_2.m
@@ -4,15 +4,28 @@ clear all; clc; close all;
 %Read in and plot the input image
 input = double(imread('lena.gif'));
 figure; 
-subplot(1,3,1); imagesc(input);
+subplot(3,3,1); imagesc(input);
 
 %Create the input mask
-mask = gaussianKernel2d(15,5);
+mask_2d = gaussianKernel2d(15,5);
+
+mask_1d_h = discreteGaussian(15,5)./discreteGaussian(15,5);
+mask_1d_v = mask_1d_h';
 
 %Apply the convolution filter
-output = filter2d(input, mask, 'replicate');
+output_2d = filter2d(input, mask_2d, 'replicate');
+output_1d_h = filter2d(input, mask_1d_h, 'replicate');
+output_1d_v = filter2d(input, mask_1d_v, 'replicate');
+
+output_h_v = filter2d(output_1d_h, mask_1d_v, 'replicate');
 
 %Plot the output
-subplot(1,3,2); imagesc(output);
-subplot(1,3,3); imagesc((output-input).^2);
+subplot(3,3,2); imagesc(output_2d);
+subplot(3,3,3); imagesc((output_2d-input).^2);
+
+subplot(3,3,5); imagesc(output_1d_h);
+subplot(3,3,6); imagesc(output_1d_v);
+
+subplot(3,3,8); imagesc(output_h_v);
+subplot(3,3,9); imagesc((output_2d - output_h_v).^2);
 colormap gray;
