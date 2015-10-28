51e4d78aff07548425a7be6158c3a881a3140664
Christian Grimm <chris-grimm@hotmail.de>
2015-10-28 16:46:11 +0100

    Delete gradOrient, updated Exercise3

--- Ex1AC/exercise3.m
+++ Ex1AC/exercise3.m
@@ -1,10 +1,19 @@
-function [ aDx, aDy, bGrad, bOrient ] = exercise3( image )
-
+%read image and convert to double type. 
+inputImage = im2double(imread('lena.gif'));
 
 aDx = [-1,0,1;-1,0,1;-1,0,1];
 aDy = [-1,-1,-1;0,0,0;1,1,1];
 
-[bGrad, bOrient] = gradOrient(image, 'mirror');
+% choose type
+type = 'mirror';
+% convolute the input image with Dx and Dy, store in to seperate images
+InConvDx = convFilter(inputImage, Dx, type);
+InConvDy = convFilter(inputImage, Dy, type);
 
-end
+% execute formula from exercise sheet to compute the gradient magnitude
+% image
+bGrad = sqrt(InConvDx.^2+InConvDy.^2);
+
+% execute formula from exercise sheet to compute the orientations image
+bOrient = atan2(InConvDx, InConvDy);
 
