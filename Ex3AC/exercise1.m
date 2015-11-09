c6b822eb5d6a59a25032e1d7b8c5e19225ffee76
Christian Grimm <chris-grimm@hotmail.de>
2015-11-09 15:29:41 +0100

    Ex 3 a) corner detector (multi-scale?)

--- Ex3AC/exercise1.m
+++ Ex3AC/exercise1.m
@@ -1,11 +1,13 @@
-image = im2double(imread('lena.gif'));
+image = im2double(imread('checkerboard_tunnel.png'));
 
 n = 3; 
-s0 = 0;
-k = 0;
-alpha = 0;
+s0 = 1;
+k = 1.4;
+alpha = 0.05;
 t = 0;
 
 output = harris(image, n, s0, k, alpha, t);
 
-imshow(output);
+imshowpair(image,output,'diff');
+%imagesc(output); colormap gray;
+%imshow(output);
--- Ex3AC/harris.m
+++ Ex3AC/harris.m
@@ -2,33 +2,48 @@ function [ outputImage ] = harris( image, n, s0, k, alpha, t )
 
 outputImage = zeros(size(image));
 
-%halfN 
-
-
-
 dx = [-1,0,1;-1,0,1;-1,0,1];
 dy = [-1,-1,-1;0,0,0;1,1,1];
 
-gaussianMask = fspecial('Gaussian', [5,5], 1);
-
-dxGau = conv2(gaussianMask, dx)
-dyGau = conv2(gaussianMask, dy);
-
-Lx = conv2(image, dxGau, 'same');
-Ly = conv2(image, dyGau, 'same');
-
-Lx2 = Lx.^2;
-Ly2 = Ly.^2;
-Lxy = Lx.*Ly;
-
-
-for x = 1:size(image,1)
-    for y = 1:size(image, 2)
+for j = 1:n
+    sigma_I = j*k*s0;
+   
+    gaussianMask = fspecial('Gaussian', [5,5], sigma_I);
     
-        mat = [Lx2(x,y), Lxy(x,y); Lxy(x,y), Ly2(x,y)]; 
-    
+
+    dxGau = conv2(gaussianMask, dx);
+    dyGau = conv2(gaussianMask, dy);
+
+    Lx = conv2(image, dxGau, 'same');
+    Ly = conv2(image, dyGau, 'same');
+
+    % elements of the matrix M
+    Lx2 = Lx.^2;
+    Ly2 = Ly.^2;
+    Lxy = Lx.*Ly;
+
+    % convolve elements of M again with a gaussian mask
+    Lx2 = conv2(Lx2, gaussianMask, 'same'); 
+    Ly2 = conv2(Ly2, gaussianMask, 'same');
+    Lxy = conv2(Lxy, gaussianMask, 'same');
+
+    for x = 1:size(image,1)
+        for y = 1:size(image, 2)
+
+            mat = [Lx2(x,y), Lxy(x,y); Lxy(x,y), Ly2(x,y)];
+
+            %mat = conv2(mat, gaussianMask, 'same');
+
+            R = det(mat) - alpha * (trace(mat) ^ 2);
+
+        if (R > t && outputImage(x,y) < R)
+             outputImage(x, y) = R; 
+        end
+
+        end
     end
-end
 
+end
+    
 end
 
