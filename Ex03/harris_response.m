a11dd2194abc0c1468c51a7704bec1730131be41
atroyn <anton.troynikov@gmail.com>
2015-11-11 14:52:04 +0100

    Fixed Harris implementation

--- Ex03/harris_response.m
+++ Ex03/harris_response.m
@@ -2,7 +2,7 @@ function response = harris_response( M , alpha )
 %HARRIS_RESPONSE computes the response of the Harris detector
 %   Using det and trace operations.
 
-response = det(M) + alpha*trace(M)^2;
+response = det(M) - alpha*(trace(M)^2);
 
 end
 
