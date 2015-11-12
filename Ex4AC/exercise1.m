848ac8270858e8947669c05e86b2361c13c9f60a
Anton Rohr <anton.rohr@gmx.de>
2015-11-12 14:54:13 +0100

    wrote mergeImages, worked

--- Ex4AC/exercise1.m
+++ Ex4AC/exercise1.m
@@ -6,17 +6,39 @@ I = im2single(rgb2gray(I));
 Ib = imread('test_shell1.jpg');
 Ib = im2single(rgb2gray(Ib));
 
-imshowpair(I, Ib, 'montage');
+imshow(mergeImages(Ib, I));
+
+
 
 
 [f,d] = vl_sift(I);
+[fb,db] = vl_sift(Ib);
 
-perm = randperm(size(f,2)) ;
-sel = perm(1:75) ;
-h1 = vl_plotframe(f(:,sel)) ;
-h2 = vl_plotframe(f(:,sel)) ;
-set(h1,'color','k','linewidth',3) ;
-set(h2,'color','y','linewidth',2) ;
+[matches, scores] = vl_ubcmatch(d, db);
 
+f(1,:) = f(1,:)+size(Ib,2);
+
+newf = zeros(4,0);
+newfb = zeros(4,0);
+
+for i = 1:size(matches, 2)
+   
+    %if scores(i) > 10000
+        
+    featureF = f(:, matches(1,i))
+    newf(:,end+1) = featureF;
+    featureFB = fb(:, matches(2,i))
+    newfb(:,end+1) = featureFB;
+    %end
+end
+
+
+%perm = randperm(size(f,2)) ;
+%sel = perm(1:75) ;
+hb = vl_plotframe(newfb) ;
+set(hb,'color','y','linewidth',2) ;
+
+h = vl_plotframe(newf);
+set(h, 'color', 'r', 'linewidth', 2);
 
 
---
+++ Ex4AC/mergeImages.m
@@ -0,0 +1,23 @@
+function [ outputImage ] = mergeImages( image1, image2 )
+%MERGEIMAGES Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+    if size(image1, 1) >= size(image2, 1)
+
+        diff = size(image1, 1) - size(image2, 1);
+        image2(end+1:end+diff,:) = zeros(diff, size(image2, 2)); 
+
+
+    else 
+
+        diff = size(image2, 1) - size(image1, 1);
+        image1(end+1:end+diff,:) = zeros(diff, size(image1, 2));
+
+    end
+
+    outputImage = [image1, image2];
+
+end
+
