65760325fba567be95b80e4633ef9634e7b41fe6
atroyn <anton.troynikov@gmail.com>
2015-11-18 19:51:21 +0100

    no message

--- Ex04/pruneMatches.m
+++ Ex04/pruneMatches.m
@@ -2,6 +2,11 @@ function [ pm_1, pm_2 ] = pruneMatches( matches_1, matches_2 )
 %PruneMatches Uses estimateGeometricTransform to prune outlier matches
 
 [tform,pm_1,pm_2] = estimateGeometricTransform(matches_1',matches_2','affine');
+
+%Dear matlab, please for the love of god be consistent in something, love
+%Anton. 
+
+
 pm_1 = pm_1';
 pm_2 = pm_2';
 
