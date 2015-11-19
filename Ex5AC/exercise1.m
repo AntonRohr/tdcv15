70485cf52c788f7ea7f002ec82122317f8d4a1c0
Anton Rohr <anton.rohr@gmx.de>
2015-11-19 17:29:54 +0100

    reworked createIntegralImage

--- Ex5AC/exercise1.m
+++ Ex5AC/exercise1.m
@@ -2,6 +2,10 @@
 % read image
 image = imread('2007_000032.jpg');
 
+integralImage = createIntegralImage(image);
+
+imagesc(integralImage);
+
 % convert lists to matrices
 [treeMatrix, leafMatrix] = getTreeMatrix('Tree0.txt');
 
