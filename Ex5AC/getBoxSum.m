e5abadb137aa3e8e267cc6ea76a41c7b80f9c0fd
Anton Rohr <anton.rohr@gmx.de>
2015-11-19 16:20:14 +0100

    wrote getBoxSum, not tested

--- Ex5AC/getBoxSum.m
+++ Ex5AC/getBoxSum.m
@@ -2,5 +2,30 @@ function [ boxSum ] = getBoxSum ( x, y, z, s )
 
 % todo
 
+% !!! this assumes x,y is upper left corner of box 
+
+%box 
+% a b 
+% c d
+
+ax = x;
+ay = y;
+
+bx = x+s;
+by = y;
+
+cx = x;
+xy = y+s;
+
+dx = x+s;
+dy = x+s;
+
+a = integralImage(ax, ay, z);
+b = integralImage(bx, by, z);
+c = integralImage(cx, cy, z);
+d = integralImage(dx, dy, z);
+
+
+boxSum = d-c-b+a;
 
 end 
