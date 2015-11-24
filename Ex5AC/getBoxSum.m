dda6ee63b26509078709959aa2a52b7bca82dda6
Anton Rohr <anton.rohr@gmx.de>
2015-11-24 14:12:11 +0100

    fixed the color mess they made in the exercise

--- Ex5AC/getBoxSum.m
+++ Ex5AC/getBoxSum.m
@@ -21,14 +21,34 @@ dx = x+s;
 dy = y+s;
 
 
-if ax < 1 || ay < 1 || dx > size(integralImage, 2) || dy > size(integralImage, 1) || z < 1
+% Matlab color channels:
+% 1 = red
+% 2 = green
+% 3 = blue
+
+% our channels
+% 0 = blue
+% 1 = green
+% 2 = red
+% 3 = red
+if z == 0
+	channel = 3; % blue 
+elseif z = 1
+	channel = 2; % green
+elseif z = 2 || z = 3
+	channel = 1; % red
+end
+
+
+
+if ax < 1 || ay < 1 || dx > size(integralImage, 2) || dy > size(integralImage, 1)
 	boxSum = 0;
 else
 
-	a = integralImage(ay, ax, z);
-	b = integralImage(by, bx, z);
-	c = integralImage(cy, cx, z);
-	d = integralImage(dy, dx, z);
+	a = integralImage(ay, ax, channel);
+	b = integralImage(by, bx, channel);
+	c = integralImage(cy, cx, channel);
+	d = integralImage(dy, dx, channel);
 
 
 	boxSum = d-c-b+a;
