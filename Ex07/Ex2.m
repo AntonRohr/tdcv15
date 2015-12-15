99af40b511a6a631b0dc68bccb4bd34977286f69
atroyn <anton.troynikov@gmail.com>
2015-12-15 11:30:45 +0100

    Completed RANSAC for each frame. Included plotMatches to easily plot matches.

--- Ex07/Ex2.m
+++ Ex07/Ex2.m
@@ -11,14 +11,25 @@ end
 
 %Do SIFT and matching in one pass
 %Should preallocate here to make it faster
-for i = 1:size(sequence,3)
+for i = 2:size(sequence,3)
     %Perform SIFT for each image
     [feature,descriptor] = vl_sift(single(sequence(:,:,i)));
     features{i} = feature;
     
     %Perform matching
-    [match, score] = vl_ubcmatch(features{i},f_0);
+    [match, score] = vl_ubcmatch(f_0,features{i});
     matches{i} = match;
 end
 
-%Compute inliers
+%Compute inliers - this breaks if we compute the inliers of the image
+%itself
+for i = 2:size(sequence,3)
+    %Get point correspondences
+    [m_0, m_i] = match_points(f_0, features{i}, matches{i});
+    %Get inliers and a homography
+    [homography, inliers_origin, inliers_target] = adaptiveRANSAC(m_0, m_i, 3, 40, 50);
+    %Store them
+    homography_i{i} = homography;
+    inliers_0{i} = inliers_origin;
+    inliers_i{i} = inliers_target;
+end
