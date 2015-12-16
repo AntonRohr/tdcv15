787d99e1c3d55e3566c1e0c111b96d97400d69bf
Nan Yang <yangnan719@gmail.com>
2015-12-16 21:09:00 +0100

    plot

--- Ex07/energy.m
+++ Ex07/energy.m
@@ -10,7 +10,7 @@ Rx = [cos(a),-sin(a),0;sin(a),cos(a),0;0,0,1];
 Ry = [cos(b),0,sin(b);0,1,0;-sin(b),0,cos(b)];
 Rz = [1,0,0;0,cos(c),-sin(c);0,sin(c),cos(c)];
 
-Rt = Rx * Ry * Rz
+Rt = Rx * Ry * Rz;
 
 Tt = [R(4); R(5); R(6)];
 
