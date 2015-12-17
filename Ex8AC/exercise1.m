ac892a7d74cfb69e5752ef26bdd2feac7b0f43c5
Anton Rohr <anton.rohr@gmx.de>
2015-12-17 12:55:26 +0100

    loaded data and images

---
+++ Ex8AC/exercise1.m
@@ -0,0 +1,8 @@
+data1 = load('data1.mat');
+data2 = load('data2.mat');
+data3 = load('data3.mat');
+
+
+faceA = rgb2gray(imread('faceA.jpg'));
+faceB = rgb2gray(imread('faceB.jpg'));
+faceC = rgb2gray(imread('faceC.jpg'));
