db8b2df1793bf26d2eb6cc50c9357ba6839a6596
Anton Rohr <anton.rohr@gmx.de>
2015-12-17 13:01:20 +0100

    fixed typos and loading

--- Ex8AC/exercise1.m
+++ Ex8AC/exercise1.m
@@ -1,9 +1,16 @@
-data1 = load('data1.mat');
+data1 = load('data1.mat', 'dat');
+data1 = data1.dat;
 data2 = load('data2.mat');
+data2 = data2.dat;
 data3 = load('data3.mat');
+data3 = data3.dat;
 
-classifiert = load('Classifiers.mat');
+classifiers = load('Classifiers.mat');
+classifiers = classifiers.classifiers;
 
 faceA = rgb2gray(imread('faceA.jpg'));
 faceB = rgb2gray(imread('faceB.jpg'));
 faceC = rgb2gray(imread('faceC.jpg'));
+
+
+%data1Pos = data1(data1(
