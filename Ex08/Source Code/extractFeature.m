18378195f4f3b2efa9b48f00a3d025d9945d7d72
atroyn <anton.troynikov@gmail.com>
2016-01-05 15:57:19 +0100

    Fixed an off-by-one error.

--- Ex08/Source Code/extractFeature.m
+++ Ex08/Source Code/extractFeature.m
@@ -1,5 +1,5 @@
 function feature = extractFeature( r,c, width, height, int_image )
-    feature = int_image(r,c) + int_image(r+height, c+width-1) - ...
+    feature = int_image(r,c) + int_image(r+height-1, c+width-1) - ...
         int_image(r, c+width-1) - int_image(r+height-1, c);
 end
 
