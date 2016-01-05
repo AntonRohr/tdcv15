23db3a3ca7d09c447f24bf5543dd552b4b1f3238
Anton Rohr <anton.rohr@gmx.de>
2016-01-05 16:29:40 +0100

    very first task of exercise 1 (visualize) and class skeleton for weak classifier

---
+++ Ex8AC/WeakClassifier.m
@@ -0,0 +1,13 @@
+classdef WeakClassifier
+    %WEAKCLASSIFIER Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Threshold
+    end
+    
+    methods
+    end
+    
+end
+
--- Ex8AC/exercise1.m
+++ Ex8AC/exercise1.m
@@ -13,4 +13,32 @@ faceB = rgb2gray(imread('faceB.jpg'));
 faceC = rgb2gray(imread('faceC.jpg'));
 
 
-%data1Pos = data1(data1(
+% split data1
+data1Pos = data1(data1(:,3) == 1, :);
+data1Neg = data1(data1(:,3) == -1, :);
+
+% split data2
+data2Pos = data2(data2(:,3) == 1, :);
+data2Neg = data2(data2(:,3) == -1, :);
+
+% split data3
+data3Pos = data3(data3(:,3) == 1, :);
+data3Neg = data3(data3(:,3) == -1, :);
+
+subplot(2,2,1);
+scatter(data1Pos(:,1), data1Pos(:,2), '+');
+hold on;
+scatter(data1Neg(:,1), data1Neg(:,2), 'o');
+hold off;
+
+subplot(2,2,2);
+scatter(data2Pos(:,1), data2Pos(:,2), '+');
+hold on;
+scatter(data2Neg(:,1), data2Neg(:,2), 'o');
+hold off;
+
+subplot(2,2,3);
+scatter(data3Pos(:,1), data3Pos(:,2), '+');
+hold on;
+scatter(data3Neg(:,1), data3Neg(:,2), 'o');
+hold off;
