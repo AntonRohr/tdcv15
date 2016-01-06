38b80619abc907c98603358d7fe8902468a2e7a2
atroyn <anton.troynikov@gmail.com>
2016-01-06 20:59:41 +0100

    Tuning

--- Ex08/Source Code/Ex2.m
+++ Ex08/Source Code/Ex2.m
@@ -1,12 +1,14 @@
+close all; clear all; clc;
+
 %% Read in the images and convert them to something useful
 faceA = double(rgb2gray(imread('data/faceA.jpg')));
 faceB = double(rgb2gray(imread('data/faceB.jpg')));
 faceC = double(rgb2gray(imread('data/faceC.jpg')));
 
 %% Compute each integral image
-int_A = getIntegralImage(faceA);
-int_B = getIntegralImage(faceB);
-int_C = getIntegralImage(faceC);
+int_A = integralImage(faceA); int_A = int_A(2:end,2:end);
+int_B = integralImage(faceB); int_B = int_B(2:end,2:end);
+int_C = integralImage(faceC); int_B = int_B(2:end,2:end);
 
 %% Read in the weak classifiers and features
 data = load('data/Classifiers.mat');
