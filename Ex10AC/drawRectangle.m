81b12be963c8fd97d9c5c1e16df621d83901185a
Anton Rohr <anton.rohr@gmx.de>
2016-01-14 15:40:40 +0100

    wrote/copied drawRectangle function

---
+++ Ex10AC/drawRectangle.m
@@ -0,0 +1,20 @@
+function [ outputImage ] = drawRectangle(rgbImg, topLeftRow, topLeftCol, botRightRow, botRightCol)
+
+topLeft = [topLeftCol, topLeftRow];
+topRight = [botRightCol, topLeftRow];
+botLeft = [topLeftCol, botRightRow];
+botRight = [botRightCol, botRightRow];
+
+line1 = [topLeft, topRight];
+line2 = [topRight, botRight];
+line3 = [botRight, botLeft];
+line4 = [botLeft, topLeft];
+
+lines = [line1;line2;line3;line4];
+
+lines = int32(lines);
+
+outputImage = insertShape(rgbImg, 'Line', lines, 'LineWidth', 2, 'Color', 'red');
+%output = insertShape(RGBimage, 'Line', lines, 'Color', color);
+
+end
