b5bff53319085191285e807fb4d912b42f739045
Christian Grimm <chris-grimm@hotmail.de>
2016-01-14 14:13:35 +0100

    Exercise1 computations and visualizations done

--- Ex10AC/exercise1.m
+++ Ex10AC/exercise1.m
@@ -1,17 +1,33 @@
-img = rgb2hsv(imread('sequence/2043_000140.jpeg'));
+% get the imageSequence
+imageSeq = readImages();
 
 
-roi = img(269:348, 481:577, 1);
+%% Exercise a) Define Region
+
+% define region of interest in the first image
+refRoi = imageSeq{1}(269:348, 481:577, 1);
+
+%% Exercise b) Calculate color histogram
+
+% plot the histogram for the first image
+refHistogram = colorHist(refRoi);
+plot(refHistogram);
+
+%% Exercise c) Calculate probability distribution
+
+pd = probMap(refRoi, refHistogram);
+mesh(pd)
 
 
-%imagesc(roi); colormap;
-
-
-histogram = colorHist(roi);
-
-%plot(histogram);
-
-pd = probMap(roi,histogram);
-
-
-mesh(pd);
+% do it for all images in the sequence for the same region of interest
+for i = 2:size(imageSeq,1)
+   roi = imageSeq{i}(269:348, 481:577, 1);
+   histogram = colorHist(roi);
+   
+   pd = probMap(roi, histogram);
+   mesh(pd);
+   
+   % use this to actually see the intermediate results
+   pause(1);
+   
+end
