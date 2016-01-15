68501a3037f5080ed6618006c51866caa6f083f2
Anton Rohr <anton.rohr@gmx.de>
2016-01-15 11:43:26 +0100

    finished exercise2

--- Ex10AC/exercise2.m
+++ Ex10AC/exercise2.m
@@ -1,27 +1,65 @@
 % imshow(imageSeq{1}(310:345, 508:555,1))
 imageSeq = readImages();
 
-% get Box that tightly embraces car (and get center from that box)
-carRegion = imageSeq{1}(310:345, 508:555,1);
-roiCenter = [ 508 + round((555-508)/2) , 310 + round((345-310) / 2) ];
-%imagesc(carRegion);
+%initial x and y (x -> col; y -> row)
+xmin = 508;
+ymin = 310;
+xmax = 555;
+ymax = 345;
 
-% get the colorHistogram of the region with the car
-carHistogram = colorHist(carRegion);
-%plot(carHistogram)
 
-currentRegion = carRegion;
 
-for i = 2:size(imageSeq,1)
-    
-    for iteration = 1:20
-        currentHist = colorHist(imageSeq{i}(currentRegion));
-        currentPd = probDist(currentRegion, currentHist);
+imshow(drawRectangle(hsv2rgb(imageSeq{1}),ymin,xmin, ymax,xmax));
+
+
+for i = 1:size(imageSeq,1)
+    tic;
+    currentImg = imageSeq{i}(:,:,1);
+    count = 0;
+    xmove = 10000;
+    ymove = 10000;
+    while count < 20 && norm([xmove,ymove]) > 2 
+        % initial roi (regoin of interest (car))
+        roi = currentImg(ymin:ymax, xmin:xmax);
+
+        roixc = xmin + round((xmax-xmin)/2);
+        roiyc = ymin + round((ymax-ymin)/2);
+
+        % get the colorHistogram of the region with the car
+        histogram = colorHist(roi);
+
+        % probabilistic density of whole image
+        pd = probMap(currentImg, histogram);
+
+        Pi = 0;
+        xPi = 0;
+        yPi = 0;
+        for x = xmin:xmax
+            for y = ymin:ymax
+                Pi = Pi + pd(y,x);
+                xPi = xPi + x*pd(y,x);
+                yPi = yPi + y*pd(y,x);
+                
+            end
+        end
+
+        xc = round(xPi/Pi);
+        yc = round(yPi/Pi);
+
+        xmove = xc-roixc;
+        ymove = yc-roiyc;
         
-        roiCenter = currentRegion;
+        xmin = xmin+(xmove);
+        xmax = xmax+(xmove);
+        ymin = ymin+(ymove);
+        ymax = ymax+(ymove);
         
-        
-    end 
-    
+        count = count +1;
+    end
+    toc;
+
+    drawedImg = drawRectangle(hsv2rgb(imageSeq{i}),ymin,xmin, ymax,xmax);
+    imshow(drawedImg);
+    imwrite(drawedImg,['result/img_', sprintf('%03d',i), '.png']);
     
 end
