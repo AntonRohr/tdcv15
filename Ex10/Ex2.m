1408c49927c7acaa8b70eaa38776b3755ea1c983
Nan Yang <yangnan719@gmail.com>
2016-01-19 08:52:20 +0100

    some names change

--- Ex10/Ex2.m
+++ Ex10/Ex2.m
@@ -16,24 +16,24 @@ for pic = 0 : 40
     y_update = 100;
     while k <= 20 && norm([x_update, y_update]) >= 2
         image_reg = image_H(ymin:ymax, xmin:xmax);
-        image_xc = round((xmax+xmin)/2);
-        imgae_yc = round((ymax+ymin)/2);
-        values = colorHist(image_reg);
-        probDist = probMap(image_H, values);        
+        geometry_xc = round((xmax+xmin)/2);
+        geometry_yc = round((ymax+ymin)/2);
+        hist = colorHist(image_reg);
+        probDist = probMap(image_H, hist);        
         sum_x = 0;
         sum_y = 0;
-        sum2 = 0; 
+        sum = 0; 
         for i = xmin : xmax
             for j = ymin:ymax
-                sum2 = sum2 + probDist(j, i);
+                sum = sum + probDist(j, i);
                 sum_x = sum_x + i * probDist(j, i);
                 sum_y = sum_y + j * probDist(j, i);
             end
         end
-        x_c = round(sum_x/sum2);
-        y_c = round(sum_y/sum2);
-        x_update = x_c - image_xc;
-        y_update = y_c - imgae_yc;
+        x_c = round(sum_x/sum);
+        y_c = round(sum_y/sum);
+        x_update = x_c - geometry_xc;
+        y_update = y_c - geometry_yc;
         xmin = xmin+x_update;
         xmax = xmax+x_update;
         ymin = ymin+y_update;
