65a4fa1a9b63fbf65773fc0544869ab8885f0b81
Christian Grimm <chris-grimm@hotmail.de>
2016-01-21 13:09:21 +0100

    Starting Last Exercise *fireworks*

---
+++ Ex11AC/ncc.m
@@ -0,0 +1,7 @@
+function [ output_args ] = ncc( input_args )
+%NCC Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ Ex11AC/ssd.m
@@ -0,0 +1,7 @@
+function [ output_args ] = ssd( input_args )
+%SSD Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
