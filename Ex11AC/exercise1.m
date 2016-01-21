1e45b4ae52bc0920e89fe6285a20701471f291f1
Anton Rohr <anton.rohr@gmx.de>
2016-01-21 16:34:38 +0100

    added drawRectangle and find minimum for SSD

--- Ex11AC/exercise1.m
+++ Ex11AC/exercise1.m
@@ -1,9 +1,22 @@
 inputImageRGB = imread('2043_000162.jpeg');
 inputImageGray = rgb2gray(inputImageRGB);
 
-templateRGB = inputImageRGB(337:400, 365:450, :);
-templateGray = inputImageGray(337:400, 365:450);
+% template measures
+topLeftRow = 337;
+topLeftCol = 365;
+botRightRow = 400;
+botRightCol = 450;
+
+templateRGB = inputImageRGB(topLeftRow:botRightRow, topLeftCol:botRightCol, :);
+templateGray = inputImageGray(topLeftRow:botRightRow, topLeftCol:botRightCol);
 
 
-scoresSSD = ssd(templateGray, inputImageGray);
-scoresNCC = ncc(im2double(templateGray),im2double(inputImageGray));
+%scoresSSD = ssd(im2double(templateGray), im2double(inputImageGray));
+%scoresNCC = ncc(im2double(templateGray),im2double(inputImageGray));
+
+[minSSDrow, minSSDcol] = find(scoresSSD==min(min(scoresSSD)),3);
+
+
+drawedImg = drawRectangle(inputImageRGB, minSSDrow, minSSDcol, minSSDrow+size(templateRGB,1), minSSDcol+size(templateRGB,2));
+
+imshow(drawedImg);
